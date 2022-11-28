63467c81097a76224c504f09ec26f2282e515516
Rishabh Bansal <rishu25bansal@gmail.com>
2022-11-28 15:59:10 +0530

    make it better

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
-import morgan from "morgan";
 import connection from "./public/DB_Connection/DB_Connection.js";
 import userRouter from "./Routes/UserRouter.js";
 const app = express();
 connection();
 let Port = 2504;
-
-
-if (process.env.NODE_ENV === "development") {
-    app.use(morgan('dev'));
-}
-
 app.use(cors());
 
 app.use(bodyParser.urlencoded({extended:true}));
